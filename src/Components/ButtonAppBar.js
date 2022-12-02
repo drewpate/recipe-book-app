e1179555ca44e1d1a3e2f0b363d750a0912f2579
Drew Pate <andrewcpate@gmail.com>
2022-12-01 22:42:02 -0500

    adds comments to button app bar

--- src/Components/ButtonAppBar.js
+++ src/Components/ButtonAppBar.js
@@ -10,15 +10,19 @@ import { useAuth } from "../Contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 const ButtonAppBar = () => {
+  //get logout function and currentUser from AuthContext
   const { logout, currentUser } = useAuth();
+  //assign and initialize useNavigate
   let navigate = useNavigate();
 
   async function handleLogOut() {
     try {
       await logout();
     } catch {
+      //TODO: look into a better validation/errorhandling
       console.log("Log Out Failed!");
     }
+    //direct user to the login page once signout is complete
     navigate("/login");
   }
 
@@ -38,7 +42,7 @@ const ButtonAppBar = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             MyRecipe Book
           </Typography>
-
+          {/* Check for currentUser, if exists then render all the buttons, if not then only render home button */}
           {currentUser ? (
             <Button color="secondary" onClick={handleLogOut}>
               Log Out
